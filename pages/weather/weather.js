242f1e1c2f83f0c366b93e35b68865deb8d8f1e1
Trim363 <360590567@qq.com>
2019-12-06 13:28:09 +0800

    Signed-off-by: Trim363 <360590567@qq.com>

---
+++ pages/weather/weather.js
@@ -0,0 +1,82 @@
+//weather.js
+// 注意：
+// 代码拷贝后，需要调整一些值，这个代码是大体过程
+
+var app = getApp();//获取当前小程序实例，方便使用全局方法和属性
+Page({
+  //1、页面数据部分
+  data:{
+    //设置页面数据，后面空值将在页面显示时通过请求服务器获取
+    cur_id:app.curid,basic:"",now:"",suggestion:"",
+    url_now:'http://127.0.0.1/wechat/xiaochengxu/t1/php/now.php', // 天气接口。提交到本地，本地curl到服务器获取返回
+    url_lifestyle:'http://127.0.0.1/wechat/xiaochengxu/t1/php/lifestyle.php', // 生活指数。提交到本地，本地curl到服务器获取返回
+    // 两个接口：对应的服务器是 "和风天气"提供的
+    api_key : 'cb5acaf12eb94aec8a0595459419cd58' // 和风天气[api-key]
+  },
+  //2、系统事件部分
+  onShow:function(){
+    var that = this;
+    wx.showToast({title: '加载中',icon: 'loading',duration: 10000})//设置加载模态框
+    that.getnow(function(d){//获取到数据的回调函数
+      wx.hideToast();
+      // console.log('getnow-d');
+      // console.log(d); return;
+      // d.now.src="http://files.heweather.com/cond_icon/"+d.now.cond.code+".png";
+      d.now.src="../../img/d01.gif"; // 这里使用外层img中的图片，固定了，如果有合适的可以替换
+      // console.log(d.basic);
+      // console.log(d.now);
+      // console.log(d.update);
+      d.basic.update = d.update;
+      that.setData({basic:d.basic,now:d.now})//更新数据，视图将同步更新
+    })
+    that.getsuggestion(function(d){
+      // console.log(d);return;
+      // console.log(d.lifestyle);
+      // console.log(d);
+      // 将数据转为合适显示的格式
+      var list = {};
+      d.lifestyle.forEach(function(item, idx, arr){
+        list[item.type] = item;
+      });
+      // console.log(list);
+      // that.setData({suggestion:d.lifestyle})//更新数据
+      that.setData({suggestion:list})//更新数据
+    })},
+  //3、自定义页面方法：获取当前天气API
+  getnow:function(fn){
+    // console.log('配置请求11');
+    var that = this;
+    // console.log(that.data.url_now);
+    wx.request({//请求服务器，类似ajax
+      url: that.data.url_now, 
+      data: {location:app.curid,key:that.data.api_key},//和风天气提供用户key，可自行注册获得
+      header: {'Content-Type': 'application/json'},
+      success: function(res) {
+        //成功后将数据传给回调函数执行
+        // console.log('获取天气：weather.js-getnow-success:');
+        // console.log(res);
+        fn(res.data.HeWeather6[0]);
+      }
+    })
+  },
+  //获取生活指数API
+  getsuggestion:function(fn){
+    // console.log('配置请求22'); return;
+    var that = this;
+    wx.request({
+      url: that.data.url_lifestyle,
+      data: {location:app.curid,key:that.data.api_key},
+      header: {'Content-Type': 'application/json'},
+      success: function(res) {
+        // console.log('获取生活指数：weather.js-getsuggestion-success:');
+        // console.log(res);
+        fn(res.data.HeWeather6[0]);
+      }
+    })
+  },
+  //4、页面事件绑定部分
+  bindViewTap:function(){wx.switchTab({url: '../city/city'})}//跳转菜单页面 
+})
+
+
+
---
+++ php/lifestyle.php
@@ -0,0 +1,150 @@
+<?php
+// http://localhost/wechat/xiaochengxu/t1/php/lifestyle.php
+// error_reporting(0);
+header('Content-Type:text/plain;charset=utf8;');
+
+// 参数整理
+$location = $_GET['location'] ?? 'beijing';
+$key = $_GET['key'] ?? 'cb5acaf12eb94aec8a0595459419cd58';
+
+// 请求地址：
+$url = 'https://free-api.heweather.net/s6/weather/lifestyle?location=' . $location . '&key=' . $key . '';
+// $rest = file_get_contents($url);
+$rest = curl($url, 0, 0, 1);
+// $arr = json_decode($rest, true);print_r($arr);exit;
+// var_dump($rest);
+echo $rest;
+
+
+
+// ============================================================
+// curl
+function curl($url, $params = false, $ispost = 0, $https = 0)
+{
+    $httpInfo = array();
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36');
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    if ($https) {
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // 对认证证书来源的检查
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); // 从证书中检查SSL加密算法是否存在
+    }
+    if ($ispost) {
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+        curl_setopt($ch, CURLOPT_URL, $url);
+    } else {
+        if ($params) {
+            if (is_array($params)) {
+                $params = http_build_query($params);
+            }
+            curl_setopt($ch, CURLOPT_URL, $url . '?' . $params); // 此处就是参数的列表,给你加了个?
+        } else {
+            curl_setopt($ch, CURLOPT_URL, $url);
+        }
+    }
+
+    $response = curl_exec($ch);
+
+    if ($response === false) {
+        // $errno = curl_errno($ch);
+        // $error = curl_error($ch);
+        //echo "cURL Error: " . curl_error($ch);
+        return false;
+    }
+    // $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    // $httpInfo = array_merge($httpInfo, curl_getinfo($ch));
+    curl_close($ch);
+    return $response;
+}
+
+
+// ============================================================
+// ============================================================
+// $rest = curl_version();//ok
+// // 发送请求 
+// $result = self::curl('网址', '参数', true); 
+// // 收到的数据需要转化一下
+//  $json = json_decode($result);
+// // 我的用法是,应为我调用的是天气预报的接口
+// $result= Curl::curl($url,"city=北京");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
